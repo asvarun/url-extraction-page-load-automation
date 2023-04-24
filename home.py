d41f75314c86dbfd4b07f3613f03dc452ca412f1
asvarun <asvprogrammer2015@gmail.com>
2023-04-24 12:53:15 +0530

    Update brwser settings 3

--- home.py
+++ home.py
@@ -5,113 +5,114 @@ import time
 from streamlit.components.v1 import html
 
 
-def table_view_choice():
-    choice = st.selectbox("Table Display", ("Grid View", "Table View"))
-    if choice == "Grid View":
-        st.dataframe(df)
-    elif choice == "Table View":
-        st.table(df)
+def main():
+    def table_view_choice():
+        choice = st.selectbox("Table Display", ("Grid View", "Table View"))
+        if choice == "Grid View":
+            st.dataframe(df)
+        elif choice == "Table View":
+            st.table(df)
+
+    st.set_page_config(layout="wide")
+
+    st.title("Nike DCU Delete Run - PIPA")
+    st.subheader("[URL Extraction & Page Load Automation]")
+
+    option = st.selectbox("Choose the Assignee", ('Anto', 'Arul', 'Arun', 'Karthik'))
+
+    if option == "Anto":
+        with open('anto.json') as file:
+            data = json.load(file)
+
+        df = pd.DataFrame(data["anto"])
+
+        table_url_data = df["table_url"].drop_duplicates()
+        table_url = list(table_url_data)
+
+        dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
+        dependent_url_data = list(dependent_dag_url)
+
+        table_view_choice()
+
+    elif option == "Arul":
+        with open('arul.json') as file:
+            data = json.load(file)
+
+        df = pd.DataFrame(data["arul"])
+
+        table_url_data = df["table_url"].drop_duplicates()
+        table_url = list(table_url_data)
+
+        dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
+        dependent_url_data = list(dependent_dag_url)
+
+        table_view_choice()
+
+    elif option == "Arun":
+        with open('arun.json') as file:
+            data = json.load(file)
+
+        df = pd.DataFrame(data["arun"])
+
+        table_url_data = df["table_url"].drop_duplicates()
+        table_url = list(table_url_data)
+
+        dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
+        dependent_url_data = list(dependent_dag_url)
+
+        table_view_choice()
+
+    elif option == "Karthik":
+        with open('karthik.json') as file:
+            data = json.load(file)
+
+        df = pd.DataFrame(data["karthik"])
+
+        table_url_data = df["table_url"].drop_duplicates()
+        table_url = list(table_url_data)
+
+        dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
+        dependent_url_data = list(dependent_dag_url)
+
+        table_view_choice()
+
+    else:
+        pass
+
+    def open_table_url():
+        for link in table_url:
+            if link:
+                url = link
+                open_script = """
+                        <script type="text/javascript">
+                            window.open('%s', '_blank').focus();
+                        </script>
+                    """ % url
+                html(open_script)
+                time.sleep(2)
+
+            else:
+                continue
+
+    def open_dependent_dag_url():
+        for link in dependent_dag_url:
+            if link:
+                url = link
+                open_script = """
+                        <script type="text/javascript">
+                            window.open('%s', '_blank').focus();
+                        </script>
+                    """ % url
+                html(open_script)
+                time.sleep(2)
+            else:
+                continue
+
+    c1, c2, c3, c4 = st.columns(4)
+
+    c2.button("Open Tables", on_click=open_table_url)
+    c3.button("Open Dependent DAGs", on_click=open_dependent_dag_url)
 
 
-st.set_page_config(layout="wide")
-
-st.title("Nike DCU Delete Run - PIPA")
-st.subheader("[URL Extraction & Page Load Automation]")
-
-option = st.selectbox("Choose the Assignee", ('Anto', 'Arul', 'Arun', 'Karthik'))
-
-if option == "Anto":
-    with open('anto.json') as file:
-        data = json.load(file)
-
-    df = pd.DataFrame(data["anto"])
-
-    table_url_data = df["table_url"].drop_duplicates()
-    table_url = list(table_url_data)
-
-    dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
-    dependent_url_data = list(dependent_dag_url)
-
-    table_view_choice()
-
-elif option == "Arul":
-    with open('arul.json') as file:
-        data = json.load(file)
-
-    df = pd.DataFrame(data["arul"])
-
-    table_url_data = df["table_url"].drop_duplicates()
-    table_url = list(table_url_data)
-
-    dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
-    dependent_url_data = list(dependent_dag_url)
-
-    table_view_choice()
-
-elif option == "Arun":
-    with open('arun.json') as file:
-        data = json.load(file)
-
-    df = pd.DataFrame(data["arun"])
-
-    table_url_data = df["table_url"].drop_duplicates()
-    table_url = list(table_url_data)
-
-    dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
-    dependent_url_data = list(dependent_dag_url)
-
-    table_view_choice()
-
-elif option == "Karthik":
-    with open('karthik.json') as file:
-        data = json.load(file)
-
-    df = pd.DataFrame(data["karthik"])
-
-    table_url_data = df["table_url"].drop_duplicates()
-    table_url = list(table_url_data)
-
-    dependent_dag_url = df["dependent_dag_url"].drop_duplicates()
-    dependent_url_data = list(dependent_dag_url)
-
-    table_view_choice()
-
-else:
-    pass
-
-
-def open_table_url():
-    for link in table_url:
-        if link:
-            url = link
-            open_script = """
-                    <script type="text/javascript">
-                        window.open('%s', '_blank').focus();
-                    </script>
-                """ % url
-            html(open_script)
-            time.sleep(2)
-
-        else:
-            continue
-
-
-def open_dependent_dag_url():
-    for link in dependent_dag_url:
-        if link:
-            url = link
-            open_script = """
-                    <script type="text/javascript">
-                        window.open('%s', '_blank').focus();
-                    </script>
-                """ % url
-            html(open_script)
-            time.sleep(2)
-        else:
-            continue
-
-
-c1, c2, c3, c4 = st.columns(4)
-
-c2.button("Open Tables", on_click=open_table_url)
-c3.button("Open Dependent DAGs", on_click=open_dependent_dag_url)
+if __name__ == "__main__":
+    main()
